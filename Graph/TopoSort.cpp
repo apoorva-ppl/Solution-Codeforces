6e6428fbf90fc06d8919e2f07e0a8d1ca9ae58be
Apoorva Pandey <148680480+apoorva-ppl@users.noreply.github.com>
2025-07-20 01:02:09 +0000

    graph

---
+++ Graph/TopoSort.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <stack>
+using namespace std;
+// private:
+class Solution {
+    void dfs(int node, vector<int>& vis, stack<int>& st, vector<vector<int>>& adj) {
+        vis[node] = 1;
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                dfs(it, vis, st, adj);
+            }
+        }
+        st.push(node);  // Push after visiting all neighbors
+    }
+
+public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // Correct declaration of adjacency list
+        vector<vector<int>> adj(V);
+        
+        // Build adjacency list from edge list
+        for (auto edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        vector<int> vis(V, 0);
+        stack<int> st;
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, vis, st, adj);
+            }
+        }
+
+        vector<int> ans;
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        return ans;
+    }
+};
